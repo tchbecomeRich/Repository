b067e2e4c508cc59bf9b5a0dec1d3fe17882df9b
3090018282@qq.com <3090018282@qq.com>
2024-12-04 09:37:00 +0800

    netty init

--- netty/src/main/java/org/socket/Server.java
+++ netty/src/main/java/org/socket/Server.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,8 +22,6 @@ import java.util.List;
 public class Server {
     public static void main(String[] args) throws IOException {
         Selector selector = Selector.open();
-
-        ByteBuffer buffer = ByteBuffer.allocate(16);
 
         ServerSocketChannel ssc = ServerSocketChannel.open();
         ssc.configureBlocking(false); // 非阻塞模式
@@ -43,15 +42,41 @@ public class Server {
         List<SocketChannel> channels = new ArrayList<>();
         while(true){
             // select没有事件发生，线程阻塞，有事件，线程恢复运行
+            // select 在事件未处理时，它不会阻塞，事件发生后要么处理，要么取消，不能置之不理
             selector.select();
             // 处理事件,selectKeys内部包含了所有发生的事件
             Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
             while(iter.hasNext()){
                 SelectionKey key = iter.next();
+                // 处理key的时候，要从selectedKeys集合中删除，否则下次会有问题
+                iter.remove();
                 log.debug("key: {}",key);
-                ServerSocketChannel channel = (ServerSocketChannel)key.channel();
-                SocketChannel sc = channel.accept();
-                log.debug("{}",sc);
+
+                // 区分类型
+                if(key.isAcceptable()){
+                    ServerSocketChannel channel = (ServerSocketChannel)key.channel();
+                    SocketChannel sc = channel.accept();
+                    sc.configureBlocking(false);
+                    SelectionKey scKey = sc.register(selector, 0, null);
+                    scKey.interestOps(SelectionKey.OP_READ);
+                    log.debug("{}",sc);
+                }else if(key.isReadable()){
+                    try{
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(4);
+                        int read = channel.read(buffer);// 如果是正常断开，read的方法返回值是-1
+                        if(read == -1){
+                            key.cancel();
+                        }else{
+                            buffer.flip();
+//                            ByteBufferUtil.debugAll(buffer);
+                            System.out.println(Charset.defaultCharset().decode(buffer));
+                        }
+                    }catch (IOException e){
+                        e.printStackTrace();
+                        key.cancel(); // 客户端断开了，因此需要将key从selector的keys集合中真正删除key
+                    }
+                }
             }
         }
     }
